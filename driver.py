c882f0bea55e3a577178a882e53f70ed326b87d9
andrevitorlima <54253998+andrevitorlima@users.noreply.github.com>
2023-10-03 21:53:32 -0300

    estudo do dimensionamento da square box

--- driver.py
+++ driver.py
@@ -8,11 +8,15 @@ def detect(gray, frame):
     faces = face_recognition.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #print (w,h)
+        # x(0;490)
+        #y(0;290)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_recognition.detectMultiScale(roi_gray, 1.1, 3)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            #print(ew,eh)
     return frame
 
 video_capture = cv2.VideoCapture(0)
