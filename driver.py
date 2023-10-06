70558658b9933e822a70aec551cff90ced10669c
andrevitorlima <54253998+andrevitorlima@users.noreply.github.com>
2023-10-06 13:51:52 -0300

    mouse moving test

--- driver.py
+++ driver.py
@@ -1,58 +1,77 @@
 import cv2
+import time
 import evdev
-from evdev import UInput, AbsInfo, ecodes as e
+import uinput
+import sys
+
+
+events = (
+        uinput.REL_X,
+        uinput.REL_Y,
+        uinput.BTN_LEFT,
+        uinput.BTN_RIGHT,
+        )
+        
 '''
 devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 for device in devices:
     print(device.path, device.name, device.phys)
 
-    /dev/input/event7 VirtualBox Webcam - HD User Fac usb-0000:00:06.0-2/button
-    /dev/input/event4 VirtualBox USB Tablet usb-0000:00:06.0-1/input0
-    /dev/input/event6 VirtualBox mouse integration 
-    /dev/input/event5 ImExPS/2 Generic Explorer Mouse isa0060/serio1/input0
-    /dev/input/event3 Video Bus LNXVIDEO/video/input0
-    /dev/input/event2 AT Translated Set 2 keyboard isa0060/serio0/input0
-    /dev/input/event1 Sleep Button LNXSLPBN/button/input0
-    /dev/input/event0 Power Button LNXPWRBN/button/input0
+/dev/input/event9 Logitech G403 HERO Gaming Mouse Keyboard usb-0000:00:06.0-3/input1
+/dev/input/event8 Logitech G403 HERO Gaming Mouse usb-0000:00:06.0-3/input0
+/dev/input/event7 VirtualBox Webcam - HD User Fac usb-0000:00:06.0-2/button
+/dev/input/event6 VirtualBox mouse integration 
+/dev/input/event5 ImExPS/2 Generic Explorer Mouse isa0060/serio1/input0
+/dev/input/event4 VirtualBox USB Tablet usb-0000:00:06.0-1/input0
+/dev/input/event3 Video Bus LNXVIDEO/video/input0
+/dev/input/event2 AT Translated Set 2 keyboard isa0060/serio0/input0
+/dev/input/event1 Sleep Button LNXSLPBN/button/input0
+/dev/input/event0 Power Button LNXPWRBN/button/input0
 '''
 
-device = evdev.InputDevice('/dev/input/event5')
-cap = {
-    e.EV_KEY : [e.KEY_A, e.KEY_B],
-    e.EV_ABS : [
-        (e.ABS_X, AbsInfo(value=0, min=0, max=255,
-                          fuzz=0, flat=0, resolution=0)),
-        (e.ABS_Y, AbsInfo(0, 0, 255, 0, 0, 0)),
-        (e.ABS_MT_POSITION_X, (0, 128, 255, 0)) ]
- }
-ui = UInput(cap, name='example-device', version=0x3)
 '''
-print(device.capabilities(verbose=True))
-{('EV_SYN', 0): [('SYN_REPORT', 0), ('SYN_CONFIG', 1), ('SYN_MT_REPORT', 2)], 
+cap = {
+   {('EV_SYN', 0): [('SYN_REPORT', 0), ('SYN_CONFIG', 1), ('SYN_MT_REPORT', 2)], 
  ('EV_KEY', 1): [(['BTN_LEFT', 'BTN_MOUSE'], 272), ('BTN_RIGHT', 273), 
 ('BTN_MIDDLE', 274), ('BTN_SIDE', 275), ('BTN_EXTRA', 276)], ('EV_REL', 2): 
 [('REL_X', 0), ('REL_Y', 1), ('REL_HWHEEL', 6), ('REL_WHEEL', 8)]}
+ }
+ui = UInput(cap, name='example-device', version=0x3)
 '''
+
+'''
+print (x,y)
+print('tamanho')
+print (w,h)
+x(0;490) ; centralizado 245
+y(0;290) ; centralizado 145
+tam medio square = 160
+teste 10%x 5%y
+
+'''
+
+delta_x = 0  
+delta_y = 0  
 
 face_recognition = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_recognition = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 
 def detect(gray, frame):
     faces = face_recognition.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        '''
+        if(x>160 or x<130 or y>100 or y<80):
+            delta_y= y-90
+            delta_x= x-145
+            with uinput.Device(events) as device:
+                for i in range(20):
+                # syn=False to emit an "atomic" (5, 5) event.
+                    device.emit(uinput.REL_X, delta_x, syn=False)
+                    device.emit(uinput.REL_Y, delta_y)
 
-        print (x,y)
-        print('tamanho')
-        print (w,h)
-         x(0;490) ; centralizado 245
-        y(0;290) ; centralizado 145
-        tam medio square = 160
-        teste 10%x 5%y
-
-        '''
+                    # Just for demonstration purposes: shows the motion. In real
+                    # application, this is of course unnecessary.
+                    time.sleep(0.01)
 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
@@ -62,15 +81,12 @@ def detect(gray, frame):
             #print(ew,eh)
     return frame
 
+
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     canvas = detect(gray, frame)
-    ui.write(e.EV_ABS, e.ABS_X, 20)
-    ui.write(e.EV_ABS, e.ABS_Y, 20)
-    ui.syn()
-    print('a')
     cv2.imshow('Video', canvas)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
