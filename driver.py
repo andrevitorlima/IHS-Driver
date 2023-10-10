105c8ee2f21911637165578461486bb374e85648
andrevitorlima <54253998+andrevitorlima@users.noreply.github.com>
2023-10-09 23:56:23 -0300

    Versão Final
    dimensionamento de tela  e ajustes para rodar em outras webcams com outras resoluções

--- driver.py
+++ driver.py
@@ -1,105 +1,85 @@
 import cv2
 import time
-import evdev
 import uinput
-import sys
 
-
+#eventos que podem ser utilizados pelo driver
 events = (
         uinput.REL_X,
         uinput.REL_Y,
         uinput.BTN_LEFT,
         uinput.BTN_RIGHT,
         )
-        
-'''
-devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-for device in devices:
-    print(device.path, device.name, device.phys)
 
-/dev/input/event9 Logitech G403 HERO Gaming Mouse Keyboard usb-0000:00:06.0-3/input1
-/dev/input/event8 Logitech G403 HERO Gaming Mouse usb-0000:00:06.0-3/input0
-/dev/input/event7 VirtualBox Webcam - HD User Fac usb-0000:00:06.0-2/button
-/dev/input/event6 VirtualBox mouse integration 
-/dev/input/event5 ImExPS/2 Generic Explorer Mouse isa0060/serio1/input0
-/dev/input/event4 VirtualBox USB Tablet usb-0000:00:06.0-1/input0
-/dev/input/event3 Video Bus LNXVIDEO/video/input0
-/dev/input/event2 AT Translated Set 2 keyboard isa0060/serio0/input0
-/dev/input/event1 Sleep Button LNXSLPBN/button/input0
-/dev/input/event0 Power Button LNXPWRBN/button/input0
-'''
-
-'''
-cap = {
-   {('EV_SYN', 0): [('SYN_REPORT', 0), ('SYN_CONFIG', 1), ('SYN_MT_REPORT', 2)], 
- ('EV_KEY', 1): [(['BTN_LEFT', 'BTN_MOUSE'], 272), ('BTN_RIGHT', 273), 
-('BTN_MIDDLE', 274), ('BTN_SIDE', 275), ('BTN_EXTRA', 276)], ('EV_REL', 2): 
-[('REL_X', 0), ('REL_Y', 1), ('REL_HWHEEL', 6), ('REL_WHEEL', 8)]}
- }
-ui = UInput(cap, name='example-device', version=0x3)
-'''
-
-'''
-print (x,y)
-print('tamanho')
-print (w,h)
-x(0;490) ; centralizado 245
-y(0;290) ; centralizado 145
-tam medio square = 160
-teste 10%x 5%y
-
-'''
-
-delta_x = 0  
-delta_y = 0  
-
+#arquivos do modelo Haar Cascade
 face_recognition = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_recognition = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-def detect(gray, frame):
+#função que cria reconhece e cria o frame do rosto e dos olhos
+def detect(gray, frame, device):
     faces = face_recognition.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        print(x,y)
-        if(x>300):
-            with uinput.Device(events) as device:
-                for i in range(20):
-                    device.emit(uinput.REL_X, -3)
-                    time.sleep(0.01)
-        if(x<180):
-            with uinput.Device(events) as device:
-                for i in range(20):
-                    device.emit(uinput.REL_X, 3)
-                    time.sleep(0.01)
-        if(y>200):
-            with uinput.Device(events) as device:
-                for i in range(20):
-                    device.emit(uinput.REL_Y, 3)
-                    time.sleep(0.01)
-        if(y<155):
-            with uinput.Device(events) as device:
-                for i in range(20):
-                    device.emit(uinput.REL_Y, -3)
-                    time.sleep(0.01)
+        #calibração manual, delimitando a altura e largura
+        #print(x, y)
+        
+        #calibração automática após os testes feitos manualmente
+        direita=(largura-w)/2
+        esquerda=(largura*1.2-w)/2
+        baixo=(altura-h)/2
+        cima=(altura*0.9-h)/2
+        
+        #calibração manual
+        #print('esquerda',esquerda)
+        #print('direita',direita)
+        #print('cima',cima)
+        #print('baixo',baixo)
+
+        #movimento do mouse
+        #ajustar a velocidade para conforto
+        if x > esquerda:
+            for i in range(20):
+                device.emit(uinput.REL_X, -2)
+                time.sleep(0.01)
+        if x < direita:
+            for i in range(20):
+                device.emit(uinput.REL_X, 2)
+                time.sleep(0.01)
+        if y > baixo:
+            for i in range(20):
+                device.emit(uinput.REL_Y, 2)
+                time.sleep(0.01)
+        if y < cima:
+            for i in range(20):
+                device.emit(uinput.REL_Y, -2)
+                time.sleep(0.01)
 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_recognition.detectMultiScale(roi_gray, 1.1, 3)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-            #print(ew,eh)
-            if len(eyes)<2:
-                device.emit(uinput.BTN_LEFT)
+            if (len(eyes) < 2) and (len(faces)==1):
+                #clique esquerdo
+                device.emit(uinput.BTN_LEFT, 1)
+                device.emit(uinput.BTN_LEFT, 0)
+
     return frame
 
-
+#criação da janela
+window_name='Video'
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 video_capture = cv2.VideoCapture(0)
-while True:
-    _, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canvas = detect(gray, frame)
-    cv2.imshow('Video', canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+altura = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+largura = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+
+with uinput.Device(events) as device:
+    while True:
+        _, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canvas = detect(gray, frame, device)
+        cv2.imshow(window_name, canvas)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 video_capture.release()
 cv2.destroyAllWindows()
